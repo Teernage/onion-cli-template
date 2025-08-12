9272e86b57c8a45911b4bc9e76c858b0df5ed25c
xuzhenxin <xuzhenxin@oray.com>
2025-08-12 14:27:17 +0800

    feat: 侧边栏插件初始化

--- src/BackgroundScript/background.ts
+++ src/BackgroundScript/background.ts
@@ -12,3 +12,14 @@ chrome.runtime.onInstalled.addListener(async () => {})
  * 新标签页创建事件
  */
 chrome.tabs.onCreated.addListener(() => {})
+
+// 监听扩展图标toggle切换侧边栏
+let isOpen = false
+
+chrome.action.onClicked.addListener(async () => {
+  isOpen = !isOpen
+
+  await chrome.sidePanel.setPanelBehavior({
+    openPanelOnActionClick: isOpen
+  })
+})
